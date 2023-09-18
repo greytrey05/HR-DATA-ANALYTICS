d0a86d5aca45d6c5daacca49ab1a1813e02672de
Graytrey05 <122462664+greytrey05@users.noreply.github.com>
2023-09-18 14:17:38 +0200

    Update script.sql

--- .github/workflows/script.sql
+++ .github/workflows/script.sql
@@ -1,2 +1,21 @@
-select *
-from 'HR RAW DATA.csv'
+/* checking the entire dataset */
+SELECT * 
+FROM `rational-logic-383310.hr.HR` 
+--------------------------------------------------
+/* Cheking for extra spaces or characters */
+SELECT trim(BusinessTravel) as trimmed_businessTravel, trim(Department) as trimmed_department
+FROM `rational-logic-383310.hr.HR` 
+---------------------------------------------------
+/* remove duplicates in the dataset */
+SELECT DISTINCT *
+FROM `rational-logic-383310.hr.HR` 
+--------------------------------------------------
+/* checking nulls entries in the dataset */
+SELECT *
+FROM
+  `rational-logic-383310.hr.HR` 
+  WHERE Department is NULL
+  OR Education is NULL
+  OR MonthlyIncome is NULL
+  OR MonthlyRate is NULL
+  OR DailyRate is NULL
