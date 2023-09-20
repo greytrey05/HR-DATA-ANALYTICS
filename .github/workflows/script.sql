56448298ee94a77a4805e8bd0e6c2dfb4ab3b228
Graytrey05 <122462664+greytrey05@users.noreply.github.com>
2023-09-20 20:03:58 +0200

    Update script.sql

--- .github/workflows/script.sql
+++ .github/workflows/script.sql
@@ -60,3 +60,15 @@ GROUP BY Department
 SELECT DISTINCT avg(MonthlyIncome) AS avg_income, min(MonthlyIncome) AS min_income, max(MonthlyIncome) as max_income, Department, Gender
 FROM `rational-logic-383310.hr.HR` 
 GROUP BY Department, Gender
+------------------------------------------------------------
+/* analyzing the criteria that demonstrate the satisfaction in each departments */
+
+SELECT distinct avg(TotalWorkingYears) as avg_totalYearsIntheCompany, avg(YearsAtCompany) as years_at_compagny, avg(WorkLifeBalance) as avg_worklifeB, avg(JobSatisfaction) as avg_jobsatisfaction, avg(JobInvolvement) as avg_jobinvolvment, avg(EnvironmentSatisfaction) as environmentsatisfaction, avg(Age) as avg_age, avg(JobLevel) as avg_jobLevel, avg(MonthlyIncome) as avg_monthly_income, avg(NumCompaniesWorked) as number_of_companyworked, Department
+FROM `rational-logic-383310.hr.HR` 
+GROUP BY Department
+------------------------------------------------------------
+/* analyzing the criteria that demonstrate the performance in each departments */
+
+SELECT distinct avg(Education) as avg_education, avg(PerformanceRating) as avg_perf_employee, avg(TrainingTimesLastYear) as avg_training_received, avg(JobInvolvement) as avg_jobinvolvment, avg(Age) as avg_age, avg(YearsAtCompany) as avg_years_at_company, avg(YearsInCurrentRole) as avg_yearsincurrentrole, Department
+FROM `rational-logic-383310.hr.HR` 
+GROUP BY Department
